520a9a4cf3975a6399a77850a7e80aa23621bd4c
Kazunori Ohtani <kazunori_ohtani@voyagegroup.com>
2013-02-06 16:50:57 +0900

    LTSVっぽくちゃんとパースするようにした

--- parse_tsv.scm
+++ parse_tsv.scm
@@ -1,15 +1,20 @@
 (use util.list)
 
-(define (update acc line)
-  (let ((key (car (string-split (car (string-split line "\t")) ":"))))
+(define (update acc expr)
+  (let ((key (car (assq-ref expr 'p))))
     (hash-table-update! acc key (cut + 1 <>) 0)
     acc))
+
+
+(define (->expr line)
+  (map (lambda (col) (map string->symbol (string-split col ":")))
+       (string-split line "\t")))
 
 (define (main args)
   (define (loop acc)
     (let ((line (read-line)))
       (if (eof-object? line)
         acc
-        (loop (update acc line)))))
+        (loop (update acc (->expr line))))))
   (print (hash-table->alist (loop (make-hash-table 'eq?))))
   0)
